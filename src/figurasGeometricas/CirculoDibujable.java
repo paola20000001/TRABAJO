2ff4c39f3dd6d350e04da48502b7d3093b054e0d
paola20000001 <Nallely.maldonado@udla.edu.ec>
2025-06-19 18:02:43 -0500

    Update CirculoDibujable.java

--- src/figurasGeometricas/CirculoDibujable.java
+++ src/figurasGeometricas/CirculoDibujable.java
@@ -1,4 +1,17 @@
 package figurasGeometricas;
+import java.awt.*;
 
-public class CirculoDibujable {
+public class CirculoDibujable extends Circulo implements figuraDibujable {
+    int x, y;
+    
+    public CirculoDibujable(double valor1, int x, int y) {
+        super(valor1);
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public void dibujar2D(Graphics g) {
+        g.drawOval(x, y, (int)valor1*2, (int)valor1*2);
+    }
 }
