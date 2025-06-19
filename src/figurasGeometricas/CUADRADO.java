b38f03829aefb539cd4234da87a8f2b90ba6a017
paola20000001 <Nallely.maldonado@udla.edu.ec>
2025-06-19 18:01:51 -0500

    Update CUADRADO.java

--- src/figurasGeometricas/CUADRADO.java
+++ src/figurasGeometricas/CUADRADO.java
@@ -1,4 +1,11 @@
 package figurasGeometricas;
 
-public class CUADRADO {
+public class Cuadrado extends Figura {
+    public Cuadrado(double valor1) { super(valor1); }
+    
+    @Override
+    public double getArea() { return valor1 * valor1; }
+    
+    @Override
+    public double getPerimetro() { return valor1 * 4; }
 }
