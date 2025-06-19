3c1b95cce473dbfff6ce039a37a6e8e0ed0c7557
paola20000001 <Nallely.maldonado@udla.edu.ec>
2025-06-19 18:02:58 -0500

    Update CuadradoDibujable.java

--- src/figurasGeometricas/CuadradoDibujable.java
+++ src/figurasGeometricas/CuadradoDibujable.java
@@ -1,11 +1,17 @@
 package figurasGeometricas;
+import java.awt.*;
 
-public class Cubo2D extends Forma {
-    public Cubo2D(double lado) { super(lado); }
-
+public class CuadradoDibujable extends Cuadrado implements figuraDibujable {
+    int x, y;
+    
+    public CuadradoDibujable(double valor1, int x, int y) {
+        super(valor1);
+        this.x = x;
+        this.y = y;
+    }
+    
     @Override
-    public double superficie() { return medida * medida; }
-
-    @Override
-    public double borde() { return medida * 4; }
+    public void dibujar2D(Graphics g) {
+        g.drawRect(x, y, (int)valor1, (int)valor1);
+    }
 }
