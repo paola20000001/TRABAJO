3ab9247c160c2ded505d82e2e138327322985557
paola20000001 <Nallely.maldonado@udla.edu.ec>
2025-06-19 18:02:09 -0500

    Update TRIANGULO.java

--- src/figurasGeometricas/TRIANGULO.java
+++ src/figurasGeometricas/TRIANGULO.java
@@ -1,4 +1,11 @@
 package figurasGeometricas;
 
-public class TRIANGULO {
+public class Triangulo extends Figura {
+    public Triangulo(double valor1) { super(valor1); }
+    
+    @Override
+    public double getArea() { return (valor1 * valor1 * Math.sqrt(3)) / 4; }
+    
+    @Override
+    public double getPerimetro() { return valor1 * 3; }
 }
